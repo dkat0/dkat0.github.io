cd8eaabc8cfa982a2e2197c6a2ab49564d4aa1ef
dkat0 <38219756+dkat0@users.noreply.github.com>
2025-01-03 05:02:35 +0000

    Deploying to gh-pages from @ dkat0/dkat0.github.io@eceb8256e10da7ed2c5f51eb56ce6f097256cadf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,8 +48,8 @@ ninja.data = [{
           window.location.href = "/blog/2023/mackey-problem/";
         
       },
-    },{id: "news-i-took-part-in-hackcmu-building-savemyhealth-an-ai-powered-personalized-health-amp-amp-longevity-recommendation-system",
-          title: 'I took part in HackCMU, building SaveMyHealth, an AI-powered personalized health &amp;amp;amp; longevity...',
+    },{id: "news-i-took-part-in-hackcmu-2024-building-savemyhealth-an-ai-powered-personalized-health-amp-amp-longevity-recommendation-system",
+          title: 'I took part in HackCMU 2024, building SaveMyHealth, an AI-powered personalized health &amp;amp;amp;...',
           description: "",
           section: "News",},{id: "news-i-m-really-excited-to-be-joining-nvidia-as-an-intern-this-summer-to-work-on-llms",
           title: 'I’m really excited to be joining NVIDIA as an intern this summer to...',
