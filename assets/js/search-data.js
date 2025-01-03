b0e7494b5c173ec7e0eb0a1fe74bd17204de451c
dkat0 <38219756+dkat0@users.noreply.github.com>
2025-01-03 02:44:07 +0000

    Deploying to gh-pages from @ dkat0/dkat0.github.io@d6e21f4ee639245b6b324c470aaf0c1df4dc1cea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,37 +79,26 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-the-mackey-problem",
+            },{id: "post-the-mackey-problem",
       
         title: "The Mackey Problem",
       
-      description: "Prove the set of injections \{0,1\}^N -&gt; R is uncountable",
+      description: "Prove that the set of injections from the set of infinite binary sequences to the real numbers is uncountable.",
       section: "Posts",
       handler: () => {
         
           window.location.href = "/blog/2023/mackey-problem/";
         
       },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
+    },{id: "post-a-distill-style-blog-post",
       
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        title: "a distill-style blog post",
       
-      description: "",
+      description: "an example of a distill-style blog post and main elements",
       section: "Posts",
       handler: () => {
         
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+          window.location.href = "/blog/2021/distill/";
         
       },
     },{id: "projects-project-1",
