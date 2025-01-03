fa634c16cc82c97983813cd8046faeb4635f84b5
dkat0 <38219756+dkat0@users.noreply.github.com>
2025-01-03 04:46:54 +0000

    Deploying to gh-pages from @ dkat0/dkat0.github.io@041c0b72ff160a298b28dd2f408cf54b65c3fdfe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Projects I&#39;ve worked on (WIP).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
